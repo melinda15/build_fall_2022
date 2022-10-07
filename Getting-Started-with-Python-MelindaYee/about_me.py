e5f9cca065e964837d896372433902794381b4be
melinda15 <melindayee15@gmail.com>
2022-10-06 18:39:35 -0700

    Completed first Python activity files

---
+++ Getting-Started-with-Python-MelindaYee/about_me.py
@@ -0,0 +1,12 @@
+name = "Melinda Yee"
+preferred_name = "Melinda"
+age = 23
+use_of_python = True
+hobbies = ["gaming", "journaling", "reading", "hiking"]
+favorite_things = {
+    "food": "sandwiches",
+    "favorite anime": "Spy x Family",
+    "favorite activity": "walking"
+}
+
+all_vars =dict(vars())
